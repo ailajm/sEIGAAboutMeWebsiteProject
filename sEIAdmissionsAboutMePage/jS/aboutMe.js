34582cf9f462fd509c438a6b3ac57639ca2829f9
Coty McKinney <59006797+cotymckinney@users.noreply.github.com>
2021-02-02 10:28:04 -0500

    Add files via upload

---
+++ sEIAdmissionsAboutMePage/jS/aboutMe.js
@@ -0,0 +1,13 @@
+//Coty McKinney SEI Assignment part 3 JavaScript file (About Me)
+
+	  	function entryWay() {
+	    	window.location='aboutMe.html';
+	    }
+
+	    function linkedLink() {
+	      	window.location='https://www.linkedin.com/in/cotymckinney/';
+	    }
+
+	    function gitLink() {
+	      	window.location='https://github.com/cotymckinney';
+	    }
